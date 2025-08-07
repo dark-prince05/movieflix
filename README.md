5bd09b8db95c153565ec1fbc0e4e1fd14707bd07
Dark-prince <barathraj0504@gmail.com>
2025-08-07 14:44:51 +0530

    Add dialog box to display detailed movie information

--- README.md
+++ README.md
@@ -3,20 +3,22 @@
 A simple web app that lets you search for movies using the [OMDb API](http://www.omdbapi.com/).  
 Built with HTML, CSS, and JavaScript
 
+Live Preview: https://dark-prince05.github.io/movieflix/
+
 # Overview
 
 ## OMDb Endpoints used
 
 - **Search Endpoint**:
   `https://www.omdbapi.com/?s={SEARCH_TERM}&apikey={YOUR_API_KEY}`  
-   Used to fetch a list of movies based on the user's search input.
+   Used to fetch a list of movies based on the user's search input.  
   This provides basic information like the movie title, release year, poster, and IMDb ID.
 
 ## Setup
 
 ### 1.Clone this repo
 
-git clone https://github.com/dark-prince05/movieflix.git
+git clone https://github.com/dark-prince05/movieflix.git  
 cd movieflix
 
 ### 2.Get your API key OMDb API
@@ -27,23 +29,24 @@ Sign up for a free key at http://www.omdbapi.com/apikey.aspx
 
 In the `app.js` file, find this line:
 
-'''js
-const response = await fetch(
-`https://www.omdbapi.com/?s=${movieName}&apikey=YOUR_API_KEY_HERE`,
-{ mode: "cors" },
+`js`
+
+const response = await fetch(  
+`https://www.omdbapi.com/?s=${movieName}&apikey=YOUR_API_KEY_HERE`,  
+{ mode: "cors" },  
 );
 
 Replace YOUR_API_KEY_HERE with the key you get from http://www.omdbapi.com/apikey.aspx
 
 ### 4.Open the app
 
-You can simply open index.html in your browser.
+You can simply open index.html in your browser.  
 Or use a simple Live Server extension in VS Code for best results.
 
 ## Usage
 
-1.Type a movie title into the input field.
-2.Click the Search button or press Enter.
+1.Type a movie title into the input field.  
+2.Click the Search button or press Enter.  
 3.A list of matching movies will appear as cards showing:
 
 - Poster (with fallback image if not available)
@@ -68,4 +71,3 @@ Or use a simple Live Server extension in VS Code for best results.
 
 - Bad API key or no internet? That would normally break the app.
 - Wrapped the fetch calls in a try-catch block and showed custom error messages like “No Internet Connection” or the one returned by OMDb.
-
--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const retrieveData = async (movieName) => {
     loading.style.display = "none"; // to hide the loading msg after the data retrieval
     if (data.Response === "True") {
       // if response is true it returns the data else display the error
-      console.log(data);
       return data;
     } else {
       displayError(data.Error);
@@ -87,6 +86,7 @@ const display = (data) => {
     movieContainer.appendChild(title);
     movieContainer.appendChild(year);
     movieContainer.classList.add("movie-container");
+    movieContainer.id = movie.imdbID;
 
     movies.appendChild(movieContainer);
   });
@@ -102,3 +102,111 @@ const displayError = (msg) => {
 
   movies.appendChild(notFound);
 };
+
+/* ----------- for detailed movie details --------------- */
+
+const dialog = document.querySelector("dialog");
+
+//this is where the detailed description of movie is retrieved using fetch api
+const retrieveDetailedData = async (movieId) => {
+  try {
+    const response = await fetch(
+      `https://www.omdbapi.com/?i=${movieId}&apikey=ab8e8f87`,
+      { mode: "cors" },
+    );
+    const data = await response.json();
+
+    if (data.Response === "True") {
+      // if response is true it returns the data else display the error
+      return data;
+    } else {
+      displayError(data.Error);
+    }
+  } catch (error) {
+    displayError("No Internet Connection");
+  }
+};
+
+// this function is used to display the more detailed movie card
+const movieDisplay = (movie) => {
+  const movieCard = document.createElement("div");
+  const movieInfo = document.createElement("div");
+  const image = document.createElement("img");
+  const title = document.createElement("div");
+  const year = document.createElement("div");
+  const director = document.createElement("div");
+  const genre = document.createElement("div");
+  const plot = document.createElement("div");
+  const runtime = document.createElement("div");
+  const ratings = document.createElement("div");
+  const language = document.createElement("div");
+  const closeBtn = document.createElement("button");
+
+  if (movie.Poster === "N/A") {
+    // if poster is empty means it will have a default placeholder image
+    image.src =
+      "https://images.unsplash.com/photo-1440404653325-ab127d49abc1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1vdmllfGVufDB8fDB8fHww";
+  } else {
+    // actual image url
+    image.src = movie.Poster;
+  }
+  image.onerror = () => {
+    // if poster isn't empty but it return 404 error or other errors, this function will handle the error
+    image.src =
+      "https://images.unsplash.com/photo-1440404653325-ab127d49abc1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1vdmllfGVufDB8fDB8fHww";
+  };
+
+  movieCard.classList.add("movie-card");
+  movieInfo.classList.add("movie-info");
+  image.alt = movie.Title;
+  image.classList.add("dialog-img");
+  title.textContent = `Title: ${movie.Title}`;
+  title.classList.add("dialog-title");
+  year.textContent = `Release Year: ${movie.Year}`;
+  year.classList.add("dialog-year");
+  director.textContent = `Director: ${movie.Director}`;
+  director.classList.add("dialog-director");
+  genre.textContent = `Genre: ${movie.Genre}`;
+  genre.classList.add("dialog-genre");
+  plot.textContent = `Plot: ${movie.Plot}`;
+  plot.classList.add("dialog-plot");
+  runtime.textContent = `Runtime: ${movie.Runtime}`;
+  runtime.classList.add("dialog-runtime");
+  ratings.textContent = `Ratings: ${movie.imdbRating}/10`;
+  ratings.classList.add("dialog-ratings");
+  language.textContent = `languages: ${movie.Language}`;
+  language.classList.add("dialog-language");
+  closeBtn.textContent = "x";
+  closeBtn.classList.add("dialog-closeBtn");
+  closeBtn.addEventListener("click", () => {
+    dialog.close();
+    movieCard.textContent = "";
+  });
+
+  movieCard.appendChild(image);
+  movieInfo.appendChild(title);
+  movieInfo.appendChild(year);
+  movieInfo.appendChild(director);
+  movieInfo.appendChild(genre);
+  movieInfo.appendChild(plot);
+  movieInfo.appendChild(language);
+  movieInfo.appendChild(runtime);
+  movieInfo.appendChild(ratings);
+  movieCard.appendChild(movieInfo);
+  movieCard.appendChild(closeBtn);
+
+  dialog.appendChild(movieCard);
+};
+
+movies.addEventListener("click", async (e) => {
+  const card = e.target.closest(".movie-container"); // this line uses event deligation to retrieve the information of the parent element even when we click on child elements
+  if (!card || !movies.contains(card)) {
+    return;
+  }
+
+  const data = await retrieveDetailedData(card.id);
+  if (data) {
+    movieDisplay(data);
+    dialog.showModal(); // this will open the dialog box and show the details of the movie
+  }
+});
